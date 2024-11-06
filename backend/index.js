0b5054b7bdb30005a88a5cfddab011dd63d8cd85
Trevor-04 <trevortopolski@icloud.com>
2024-11-06 14:37:06 -0600

    changing around some backend stuff

--- backend/index.js
+++ backend/index.js
@@ -85,9 +85,25 @@ app.use("/donations", donationRoutes);
 //   console.log(`Server is running on ${url} and port ${serverPort}`);
 // });
 
-const PORT = process.env.PORT || 3000; 
 
 // Start the server
-app.listen(PORT, async () => {
-  console.log(`Server is running on ${process.env.PORT}`);
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
 });
