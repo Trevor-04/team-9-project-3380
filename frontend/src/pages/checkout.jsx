a7eac553c848d7edf9837da770c68e4c65cccebf
Trevor-04 <trevortopolski@icloud.com>
2024-11-13 23:08:15 -0600

    fixing default import for jwtdecode

--- frontend/src/pages/checkout.jsx
+++ frontend/src/pages/checkout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 function Checkout() {
     const location = useLocation();
