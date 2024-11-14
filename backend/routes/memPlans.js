17f723455fab63e5266919d60519e497820aca10
Trevor-04 <trevortopolski@icloud.com>
2024-11-13 18:27:03 -0600

    fixing bugs

--- backend/routes/memPlans.js
+++ backend/routes/memPlans.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const donationsController = require('../functions/memPlans');
+const memPlansController = require('../functions/memPlans');
 
 router.get('/add', async (req, res) => {
     try {
         const planData = req.body; // Get data from request body
-        const result = await donationsController.addPlan(planData);
+        const result = await memPlansController.addPlan(planData);
         res.status(201).json({ message: 'Plan added successfully!', result });
     } catch (error) {
         console.error("Error adding plan:", error);
